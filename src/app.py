668d090c588ad223717738ee5b4982de65466d5a
Donald-Mase <donaldmutai@proton.me>
2023-12-18 05:11:39 -0800

    Update app.py

--- src/app.py
+++ src/app.py
@@ -9,6 +9,8 @@ import pandas as pd
 import folium
 from folium import plugins
 import dash_bootstrap_components as dbc
+import plotly.graph_objects as go
+import openpyxl
 
 app = Dash(__name__, title="dd1")
 
@@ -21,34 +23,11 @@ csv_link = 'https://raw.githubusercontent.com/Donald-Mutai/Files/main/wfp_food_p
 df = pd.read_csv(csv_link)
 data = pd.read_csv(csv_link)
 
-def load_data(data_file: str) -> pd.DataFrame:
-    '''
-    Load data from /data directory
-    '''
-    PATH = pathlib.Path(__file__).parent
-    DATA_PATH = PATH.joinpath("data").resolve()
-    return pd.read_csv(DATA_PATH.joinpath(data_file))
-
 app.layout = html.Div([
     html.H4('Implement from here'),
     html.Button("Switch Axis", n_clicks=0,
                 id='button'),
 ])
-
-@app.callback(
-    Output("graph", "figure"),
-    Input("button", "n_clicks"))
-def display_graph(n_clicks):
-    # replace with your own data source
-    df = load_data("2014_apple_stock.csv")
-
-    if n_clicks % 2 == 0:
-        x, y = 'AAPL_x', 'AAPL_y'
-    else:
-        x, y = 'AAPL_y', 'AAPL_x'
-
-    fig = px.line(df, x=x, y=y)
-    return fig
 
 
 if __name__ == "__main__":
